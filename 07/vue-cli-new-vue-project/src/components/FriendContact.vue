c9aa34b18007a7894020e846d985e96d85392bfe
jeff <jeffery.lemar.graham@gmail.com>
2022-02-26 21:06:54 -0500

    add google fonts

---
+++ 07/vue-cli-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ buttonMsg }} Details</button>
+    <ul v-if="showingDetails">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showingDetails: false,
+      friend: {
+        id: 'jeff',
+        name: 'Jeff Graham',
+        phone: '123456789',
+        email: 'jeff@test.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.showingDetails = !this.showingDetails;
+    },
+  },
+  computed: {
+    buttonMsg() {
+      return this.showingDetails ? 'Hide' : 'Show';
+    },
+  },
+};
+</script>
